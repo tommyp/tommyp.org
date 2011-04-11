b61313372e22c85ba0350b40415132ecc8e19536
Tommy Palmer <hi@tommyp.org>
2011-04-11 11:59:33 +0100

    starting theme

---
+++ themes/journal/README.md
@@ -0,0 +1,7 @@
+Journal Nesta theme
+===================
+
+journal is a theme for Nesta, a [Ruby CMS](nesta), designed by
+<insert your name here>.
+
+[nesta]: http://effectif.com/nesta
---
+++ themes/journal/app.rb
@@ -0,0 +1,22 @@
+# Use the app.rb file to load Ruby code, modify or extend the models, or
+# do whatever else you fancy when the theme is loaded.
+
+module Nesta
+  class App
+    # Uncomment the Rack::Static line below if your theme has assets
+    # (i.e images or JavaScript).
+    #
+    # Put your assets in themes/journal/public/journal.
+    #
+    use Rack::Static, :urls => ["/journal"], :root => "themes/journal/public"
+
+    helpers do
+      # Add new helpers here.
+    end
+
+    get '/css/:sheet.css' do
+          content_type 'text/css', :charset => 'utf-8'
+          cache scss(params[:sheet].to_sym)
+    end
+  end
+end
