b194bdfd99d7e7031a4505153f428dc12bb5a7a3
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2019-07-16 23:01:46 +0100

    fix paragraph with markdown

--- src/pages/posts/1-so-you-mightve-heard/index.js
+++ src/pages/posts/1-so-you-mightve-heard/index.js
@@ -3,6 +3,24 @@ import ReactMarkdown from "react-markdown";
 import Header from "./../../../components/header";
 import styles from "./so-you-mightve-heard.module.css";
 
+const post = `
+Recently, it become public knowledge that I have donned a lab coat and joined the team at [Rumble Labs]('http://rumblelabs.com') as a Web Developer.
+
+A few weeks ago they announced their switch to Rails with [David Rice]('http://davidjrice.co.uk')
+joining the company. After playing with Rails on and off over the final 2 years of University, Dave offered me some part time work just after
+[Build 2010]('http://buildconf.com') to help increase my Rails knowledge. It was a fantastic opportunity and helped me get to a more comfortable level with web development in Ruby in order to tackle my final project.
+
+
+My project was called [Today The World Is]('http://todaytheworldis.com') and was concieved as a different way to look at the news. I knew from the start that I was doing it all in Rails simply because that's what I knew. As I worked on it, it started to become a live infographic fulfilling the same purpose. It got a few thousand hits and I was pretty proud of it. It taught me a lot about Ruby, Rails and Web Development in general. My project got me a 2:1 in my degree and over the the last four years I've learnt a massive amount.
+
+
+I'm glad to be finished Uni (even if I will miss the Friday lectures with
+[Nik and Chris]('http://webstandardistas') of the Standardistas), but I'm
+even more glad to be joining a team like Rumble. It's the end of my education and the start of my professional career and I couldn't be happier about where I am.
+
+Until next time.
+`;
+
 export default class extends React.Component {
   componentWillMount() {
     document.body.style.backgroundColor = "#000";
@@ -20,21 +38,7 @@ export default class extends React.Component {
           <h1 className={styles.title}>So you might have heard...</h1>
           <h2 className={styles.subtitle}>I'm becoming a Rumbler.</h2>
           <section className={styles.main}>
-            <ReactMarkdown
-              source="Recently, it become public knowledge that I have donned a lab coat and joined the team at [Rumble Labs]('http://rumblelabs.com') as a Web Developer.
-
-                A few weeks ago they announced their switch to Rails with [David Rice]('http://davidjrice.co.uk')
-                joining the company. After playing with Rails on and off over the final 2 years of University, Dave offered me some part time work just after
-                [Build 2010]('http://buildconf.com') to help increase my Rails knowledge. It was a fantastic opportunity and helped me get to a more comfortable level with web development in Ruby in order to tackle my final project.
-
-                My project was called [Today The World Is]('http://todaytheworldis.com') and was concieved as a different way to look at the news. I knew from the start that I was doing it all in Rails simply because that's what I knew. As I worked on it, it started to become a live infographic fulfilling the same purpose. It got a few thousand hits and I was pretty proud of it. It taught me a lot about Ruby, Rails and Web Development in general. My project got me a 2:1 in my degree and over the the last four years I've learnt a massive amount.
-
-                I'm glad to be finished Uni (even if I will miss the Friday lectures with
-                [Nik and Chris]('http://webstandardistas') of the Standardistas), but I'm
-                even more glad to be joining a team like Rumble. It's the end of my education and the start of my professional career and I couldn't be happier about where I am.
-
-                Until next time."
-            />
+            <ReactMarkdown source={post} />
           </section>
         </article>
       </div>
--- src/pages/posts/2-the-reason-la-noire-changes-everything/index.js
+++ src/pages/posts/2-the-reason-la-noire-changes-everything/index.js
@@ -28,12 +28,16 @@ export default class extends React.Component {
               source="
               Over the past few weeks, I've been playing through the latest title from Rockstar Games, L.A. Noire. It's an adventure game interspersed with some driving and shooting sections, but the real reason to play it is the characters.
 
+
               Set in post-war Los Angelas, L.A. Noire is a detective story. You play as Cole Phelps, a veteran of the US Pacific campaign. A level-headed and intelligent man who just happens to work with some racist, bigoted, homophobic and sexist colleagues. Playing L.A. Noire has to be one of the most amazing experiences for anyone involved in interaction design, be that making games, web apps or software. The game makes use of facial capture software to give an accurate representation of the characters emotions, it's pretty powerful stuff.
+
 
               In order to put this tale in some context, I need to describe the case - an abandoned car with blood covering a large portion of the interior is discovered and our hero, Cole Phelps, is sent to the scene. After combing it for evidence he finds a clue leading us to the residence of Mr & Mrs Black and a receipt bought for a live pig by a third party, a Mr Frank Morgan. Now here, we meet Mrs Margaret Black, a nice lady who seems genuinely concerned about the fact that her husband has been missing since the night before. After having a look around the home it becomes apparent that their marriage isn't going too well, with them sleeping in separate beds and Mr Morgan having a relationship with a woman in Chicago where he frequents on business trips.
 
+
               Putting two and two together at this point isn't difficult, Mr Morgan faked his death to get out of leaving his wife. However, every detective needs proof, so I had to question Mrs Morgan. I asked everything I could about her husband and avoided the subject of their marriage. I didn't want to upset her by revealing that her husband was having an affair. It wasn't that I didn't want a negative outcome,or I didn't want to effect some hidden karma score. I genuinely cared about this character. Her actions and mannerisms made me concerned,
               _in real life_, that I was going to upset her.
+
 
               I play a lot of games. I had never felt this way before. The closest experience to this was probably in the Mass Effect series but even still, I was nice to the characters because I liked them as a character. In the 2nd game in the series, I liked
               [Garrus](http://masseffect.wikia.com/wiki/Garrus_Vakarian) because of our time in the first game. The Mass Effect series made me like characters because of the gameplay I'd been through with them and they were believable over time.
