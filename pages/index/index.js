5b5a7309dc61e7b98d49e698c9e3a8cb887499c2
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2020-03-28 16:50:47 +0000

    delete everything

---
+++ pages/index/index.js
@@ -0,0 +1,12 @@
+function Index() {
+  return (
+    <section className="index">
+      <p>
+        I'm Tommy Palmer, a Software Engineer living in London and working at{" "}
+        <a href="https://deliveroo.co.uk/">Deliveroo</a>.
+      </p>
+    </section>
+  );
+}
+
+export default Index;
