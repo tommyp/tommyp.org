b245478934089884c0fb87609ce8d9828fb25bd2
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2020-04-12 00:29:11 +0100

    pass text and border color in as props to header

--- components/header.js
+++ components/header.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Link from "next/link";
+import PropTypes from "prop-types";
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      borderColor: props.borderColor || "border-black",
+      textColor: props.textColor || "text-black",
+    };
+  }
   render() {
     return (
-      <header className="font-header mx-auto sm:w-full md:w-5/6 pt-4 pb-2 pl-3 pr-3 border-black border-b-2 border-solid">
+      <header
+        className={`font-header mx-auto sm:w-full md:w-5/6 pt-4 pb-2 pl-3 pr-3 ${this.state.borderColor} border-b-2 border-solid`}
+      >
         <nav className="container mx-auto">
           <h1 className="inline text-xl md:text-3xl mr-20">
             <Link href="/">
@@ -26,3 +37,8 @@ export default class Header extends React.Component {
     );
   }
 }
+
+Header.propTypes = {
+  borderColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
