db84c7faa3bf6c675fbe40ddb5de51256504c1c5
Tommy Palmer <tommy.palmer@deliveroo.co.uk>
2020-04-11 23:42:04 +0100

    add photo to homepage

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,21 @@ function Index() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header />
-      <section className="container mx-auto w-4/6 font-sans">
-        <p className="sm:text-6xl lg:text-6xl mt-20 text-gray-800 p-4 rounded-lg">
-          I’m Tommy Palmer a Software Engineer living in London and working at{" "}
+      <section className="container mx-auto w-5/6 font-sans">
+        <p className="sm:text-3xl lg:text-4xl mt-32 text-gray-800 rounded-lg w-2/3 float-left pl-16 float-left">
+          I’m Tommy Palmer, a Software Engineer living in London and working at{" "}
           <a
             href="https://deliveroo.co.uk/"
-            className="border-b-8 border-solid border-teal-400 text-gray-800 hover:text-teal-400"
+            className="border-b-4 border-solid border-teal-400 text-gray-800 hover:text-teal-400"
           >
             Deliveroo
           </a>
           .
         </p>
+        <img
+          className="float-right w-64 h-64 mr-32 mt-20 rounded-full "
+          src="/images/bangface.jpg"
+        />
       </section>
     </React.Fragment>
   );
