7cdf033dc89897bd08b78e62a45bb6053dc03ee7
Tommy Palmer <hi@tommyp.org>
2020-04-21 23:35:06 +0100

    add wtf to work

--- pages/work.js
+++ pages/work.js
@@ -37,6 +37,30 @@ export default class Work extends React.Component {
           />
         </Head>
         <Header />
+        <section className="w-screen text-black">
+          <section className="container w-5/6 mx-auto pt-10 pb-10">
+            <section className="w-1/2 float-left pr-10">
+              <h2 className="text-6xl font-sans font-bold">
+                <a href="https://www.whatthefuckshouldido.in">
+                  WHAT THE FUCK SHOULD I DO IN?
+                </a>
+              </h2>
+              <p className="text-xl pt-10">
+                I&apos;d played with <a href="https://elixir-lang.com">Elixir</a> a little bit, but decided during an on-call
+                shift to build something over the week with it. Being inspired by <a href="https://whatthefuckshouldimakefordinner.com/">another site</a> I wanted to build a sweary travel guide.
+              </p>
+              <p className="text-xl pt-10">
+                I like building projects that mash APIs together in fun ways and I also like eating and drinking my way through cities, so it was a really fun side project.
+              </p>
+            </section>
+            <section className="w-1/2 float-right pl-10 mt-20">
+              <a href="https://www.whatthefuckshouldido.in">
+                <img src="/images/work/wtf.png" />
+              </a>
+            </section>
+            <div className="clearfix" />
+          </section>
+        </section>
         <section className="w-screen bg-teal-400 text-purple-900">
           <section className="container w-5/6 mx-auto pt-10 pb-10">
             <section className="w-1/2 float-left pr-10">
@@ -60,9 +84,6 @@ export default class Work extends React.Component {
             <div className="clearfix" />
           </section>
         </section>
-        {/* <section className="container mx-auto w-5/6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 pt-20">
-          {workCards}
-        </section> */}
       </React.Fragment >
     );
   }
