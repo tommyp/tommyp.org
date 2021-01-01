7323b413393df27b0610e7c81572f86569bb2ef5
Tommy Palmer <hi@tommyp.org>
2021-01-01 09:55:54 +0000

    make homepage flex

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,14 @@ function Index() {
     <React.Fragment>
       <Head/>
       <Header />
-      <section className="container mx-auto w-5/6 lg:pl-16 font-mono">
-        <div className="w-full lg:float-right lg:w-1/3 mt-10 lg:mt-20 ">
+      <section className="container mx-auto w-5/6 lg:pl-16 font-mono flex flex-row-reverse">
+        <div className="w-full lg:w-1/3 mt-10 lg:mt-20 ">
           <img
             className="w-32 h-32 md:w-64 md:h-64 rounded-full mx-auto lg:ml-0"
             src="/images/wall.jpg"
           />
         </div>
-        <p className="md:text-3xl lg:text-4xl mt-8 lg:mt-32 text-black rounded-lg w-full lg:w-2/3 lg:float-left lg:pr-16 float-left text-center lg:text-left">
+        <p className="md:text-3xl lg:text-4xl mt-8 lg:mt-32 text-black rounded-lg w-full lg:w-2/3 lg:pr-16 text-center lg:text-left">
           I’m Tommy Palmer, a Software Engineer living in London and working at{" "}
           <ExtA
             href="https://deliveroo.co.uk/"
@@ -27,7 +27,6 @@ function Index() {
           </ExtA>
           .
         </p>
-        <div className="clearfix" />
       </section>
     </React.Fragment>
   );
