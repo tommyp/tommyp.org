6328fa39e2285f61aba51ae05acdf17ad31eefe5
Tommy Palmer <hi@tommyp.org>
2021-01-02 19:00:14 +0000

    add opengraph attrs

--- components/head.js
+++ components/head.js
@@ -1,10 +1,14 @@
 import Head from "next/head";
 import PropTypes from "prop-types";
 
-const AppHead = ({title}) => {
+const AppHead = ({ title }) => {
+  title = title ? `${title} | Tommy Palmer` : 'Tommy Palmer | A Software Engineer living in London'
+
   return (
     <Head>
-      <title>{title ? `${title} | Tommy Palmer` : 'Tommy Palmer | A Software Engineer living in London'}</title>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="article" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
   )
