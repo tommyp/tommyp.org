d2325f23be0a699facdf1a60c2e73f1acf18b0af
Tommy Palmer <hi@tommyp.org>
2021-08-24 15:25:12 +0100

    add splitbee

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,30 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html>
         <Head />
-        <script async defer data-domain="tommyp.org" src="https://plausible.io/js/plausible.js"></script>
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
+
+        <script
+          async
+          defer
+          data-domain="tommyp.org"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
