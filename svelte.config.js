08d2435c07bff1c8dfa048b8f9ec051f017ea2fb
Tommy Palmer <hi@tommyp.org>
2022-03-13 21:32:20 +0000

    use svelte netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { isoImport } from 'vite-plugin-iso-import';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+
 		vite: {
 			plugins: [isoImport()]
 		}
