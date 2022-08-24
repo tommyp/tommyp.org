9eeebb48f271d72f6c7f3570e5a122aa02036ce8
Tommy Palmer <hi@tommyp.org>
2022-08-24 09:13:43 +0100

    basic page setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>I'm a footer</footer>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Blog</h1>
+
+<p>I really should write something...</p>
