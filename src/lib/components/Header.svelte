c75d498f442d62b28da3eb910da78900e20d9eb3
Tommy Palmer <hi@tommyp.org>
2022-08-31 09:30:50 +0100

    style header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,7 @@
 <header>
-	<a href="/">Home</a>
+	<h1>
+		<a href="/">Tommy Palmer</a>
+	</h1>
 
 	<nav>
 		<ul>
@@ -18,22 +20,56 @@
 
 <style>
 	header {
-		padding: 1rem;
-		background: lightskyblue;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
+		text-align: center;
+		font-size: 1.5rem;
+	}
+
+	h1 {
+		border-bottom: 4px solid var(--border-color);
+		margin: 0;
+	}
+
+	h1 > a {
+		text-decoration: none;
+		color: inherit;
+		display: block;
+		padding: 2rem 0;
 	}
 
 	ul {
 		margin: 0;
+		padding: 0;
 		list-style-type: none;
 		display: flex;
-		gap: 1rem;
+
+		width: 100vw;
+		justify-content: space-between;
 	}
 
-	a {
+	li {
+		list-style: none;
+		padding: 0;
+		display: block;
+		width: 100%;
+	}
+
+	li > a {
 		text-decoration: none;
 		color: inherit;
+		display: block;
+		padding: 1rem 0;
+		border-bottom: 4px solid var(--border-color);
+		border-right: 4px solid var(--border-color);
+		font-family: 'Karrik', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	li:last-child > a {
+		border-right: none;
+	}
+
+	a:hover {
+		color: var(--background);
+		background: var(--foreground);
 	}
 </style>
