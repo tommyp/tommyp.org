5b032b3887e95bb4bda4ae5b3aea28f318e49b71
Tommy Palmer <hi@tommyp.org>
2022-11-05 20:41:34 +0000

    change background

--- src/lib/components/graphics/Background.svelte
+++ src/lib/components/graphics/Background.svelte
@@ -18,7 +18,7 @@
 			radial-gradient(at 81% 32%, hsla(226, 62%, 75%, 1) 0px, transparent 50%),
 			radial-gradient(at 26% 80%, hsla(64, 85%, 70%, 1) 0px, transparent 50%),
 			radial-gradient(at 25% 4%, hsla(101, 82%, 74%, 1) 0px, transparent 50%),
-			radial-gradient(at 50% 81%, hsla(281, 87%, 75%, 1) 0px, transparent 50%),
+			radial-gradient(at var(--mouse-x) var(--mouse-y), hsla(281, 87%, 75%, 1) 0px, transparent 50%),
 			radial-gradient(at 51% 90%, hsla(253, 65%, 75%, 1) 0px, transparent 50%),
 			radial-gradient(at 86% 57%, hsla(322, 77%, 72%, 1) 0px, transparent 50%);
 	}
