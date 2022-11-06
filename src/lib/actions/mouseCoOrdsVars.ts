efbae9db06222a295f6f9cfdf4586531af307d10
Tommy Palmer <hi@tommyp.org>
2022-11-06 18:04:16 +0000

    animate background

--- src/lib/actions/mouseCoOrdsVars.ts
+++ src/lib/actions/mouseCoOrdsVars.ts
@@ -1,10 +1,12 @@
-export const mouseCoOrdsVars = (node: HTMLElement) => {
+import type Tweened from 'svelte/motion';
+
+export const mouseCoOrdsVars = (node: HTMLElement, coords: Tweened<{ x: number; y: number }>) => {
 	const handleMouseMove = (e) => {
 		const { clientX, clientY } = e;
 		const { left, top } = node.getBoundingClientRect();
 		const x = clientX - left;
 		const y = clientY - top;
-		node.setAttribute('style', `--mouse-x: ${x}px; --mouse-y: ${y}px;`);
+		coords.set({ x, y });
 	};
 
 	document.addEventListener('mousemove', handleMouseMove);
--- src/lib/components/graphics/Background.svelte
+++ src/lib/components/graphics/Background.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
 	import { mouseCoOrdsVars } from '$lib/actions/mouseCoOrdsVars';
+	import { cubicOut } from 'svelte/easing';
+	import { tweened, type Tweened } from 'svelte/motion';
+	let x: number = 0.5;
+	let y: number = 0.5;
+
+	const coords: Tweened<{ x: number; y: number }> = tweened(
+		{ x, y },
+		{
+			duration: 2000,
+			easing: cubicOut
+		}
+	);
+
+	$: percantages = {
+		x: (($coords.x / window.innerWidth) * 100).toFixed(0),
+		y: (($coords.y / window.innerHeight) * 100).toFixed(0)
+	};
 </script>
 
-<div class="canvas" use:mouseCoOrdsVars />
+<div
+	class="canvas"
+	use:mouseCoOrdsVars={coords}
+	style={`--mouse-x: ${percantages.x}%; --mouse-y: ${percantages.y}%;`}
+/>
 
 <style>
 	.canvas {
-		--x-percent: calc(var(--mouse-x));
 		z-index: -1;
 		position: fixed;
 		top: 0;
@@ -16,10 +36,18 @@
 		background-color: #99e4ff;
 		background-image: radial-gradient(at 52% 0%, hsla(38, 69%, 75%, 1) 0px, transparent 50%),
 			radial-gradient(at 81% 32%, hsla(226, 62%, 75%, 1) 0px, transparent 50%),
-			radial-gradient(at 26% 80%, hsla(64, 85%, 70%, 1) 0px, transparent 50%),
+			radial-gradient(
+				at calc(100% - var(--mouse-y)) calc(100% - var(--mouse-x)),
+				hsla(64, 85%, 70%, 1) 0px,
+				transparent 50%
+			),
 			radial-gradient(at 25% 4%, hsla(101, 82%, 74%, 1) 0px, transparent 50%),
 			radial-gradient(at var(--mouse-x) var(--mouse-y), hsla(281, 87%, 75%, 1) 0px, transparent 50%),
 			radial-gradient(at 51% 90%, hsla(253, 65%, 75%, 1) 0px, transparent 50%),
-			radial-gradient(at 86% 57%, hsla(322, 77%, 72%, 1) 0px, transparent 50%);
+			radial-gradient(
+				at calc(100% - var(--mouse-x)) calc(100% - var(--mouse-y)),
+				hsla(322, 77%, 72%, 1) 0px,
+				transparent 50%
+			);
 	}
 </style>
