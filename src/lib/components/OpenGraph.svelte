18185ae7e91214e74bd66767d8442dda2232bbc2
Tommy Palmer <hi@tommyp.org>
2022-11-14 22:04:49 +0000

    add css

---
+++ src/lib/components/OpenGraph.svelte
@@ -0,0 +1,15 @@
+<div style="color: red">Hello, world!</div>
+
+<style>
+	div {
+		font-family: 'DM Sans';
+		display: flex;
+		flex-direction: column;
+		font-size: 50px;
+		background-color: white;
+		height: 100%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ src/routes/og/+server.ts
@@ -0,0 +1,44 @@
+import { html as toReactNode } from 'satori-html';
+import { Resvg } from '@resvg/resvg-js';
+import DMSans from '$lib/fonts/DMSans-Bold.ttf';
+import OpenGraph from '$lib/components/OpenGraph.svelte';
+import satori from 'satori';
+import type { RequestHandler } from './$types';
+import type { SvelteComponent } from 'svelte';
+
+const height = 630;
+const width = 1200;
+
+export const GET: RequestHandler = async () => {
+	const result: SvelteComponent = OpenGraph.render();
+	console.log(result);
+
+	const element = toReactNode(`${result.html}<style>${result.css.code}</style>`);
+
+	const svg = await satori(element, {
+		fonts: [
+			{
+				name: 'DM Sans',
+				data: Buffer.from(DMSans),
+				style: 'normal'
+			}
+		],
+		height,
+		width
+	});
+
+	const resvg = new Resvg(svg, {
+		fitTo: {
+			mode: 'width',
+			value: width
+		}
+	});
+
+	const image = resvg.render();
+
+	return new Response(image.asPng(), {
+		headers: {
+			'content-type': 'image/png'
+		}
+	});
+};
