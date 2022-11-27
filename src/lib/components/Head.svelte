b32b4af2e8bf0112a79ca2a77ddea3d37c984abe
Tommy Palmer <hi@tommyp.org>
2022-11-27 12:08:56 +0000

    use url search params

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -4,16 +4,15 @@
 	export let prefix: string = undefined;
 	export let color: string = undefined;
 
-	let ogParams: string;
+	const ogParams: URLSearchParams = new URLSearchParams();
 	if (prefix) {
-		ogParams = `?heading=${escape(prefix)}&color=${color.replace('#', '')}`;
-	} else {
-		ogParams = '';
+		ogParams.set('prefix', escape(prefix));
+		ogParams.set('color', color.replace('#', ''));
 	}
 </script>
 
 <svelte:head>
-	<meta property="og:image" content={`https://tommyp.org/og${ogParams}`} />
-	<meta property="twitter:image" content={`https://tommyp.org/og${ogParams}`} />
+	<meta property="og:image" content={`https://tommyp.org/og?${ogParams.toString()}`} />
+	<meta property="twitter:image" content={`https://tommyp.org/og?${ogParams.toString()}`} />
 	<title>{prefix ? `${prefix} | ` : ''}Tommy Palmer | Web Developer based in London</title>
 </svelte:head>
