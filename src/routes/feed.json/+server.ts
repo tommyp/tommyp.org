2f6e36935b1f526d6861093b2cf0215c71f40525
Tommy Palmer <hi@tommyp.org>
2022-12-18 18:41:11 +0000

    add basic rss feed

--- src/routes/feed.json/+server.ts
+++ src/routes/feed.json/+server.ts
@@ -1,13 +1,11 @@
 export const prerender = true;
 import type { RequestHandler } from '@sveltejs/kit';
 import type { Post } from '$lib/types';
-import LZString from 'lz-string';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import postsjson from '$generated/posts.json';
 import { fetchMarkdownPosts } from '$lib/utils';
 
-const _allposts = fetchMarkdownPosts().then((posts) => {
+const allposts = await fetchMarkdownPosts().then((posts) => {
 	return posts.sort((a, b) => {
 		return new Date(b.meta.date).getTime() - new Date(a.meta.date).getTime();
 	});
@@ -30,34 +28,34 @@ const render = async () => {
 			}
 		],
 		language: 'en',
-		items: _allposts.map((e) => {
+		items: allposts.map((post) => {
+			// console.log(post);
+
 			return {
-				id: e[1].slug,
-				url: `${new URL(`${e[1].slug}`, siteConfig.url).href}`,
-				title: e[1].title,
-				summary: e[1].summary,
-				image: e[1].cover,
-				date_published: e[1].published,
-				date_modified: e[1].updated,
-				content_text: e[1].content && LZString.decompressFromBase64(e[1].content),
-				content_html: e[1].html && LZString.decompressFromBase64(e[1].html),
-				tags: e[1].tags
-					?.map((c: string | string[] | { string: string } | { string: string[] }) => {
-						if (typeof c === 'string') return c;
-						if (Array.isArray(c)) {
-							return c.map((v) => {
-								return v;
-							});
-						}
-						const [key, value] = Object.entries(c)[0];
-						if (Array.isArray(value)) {
-							return value.map((v) => {
-								return `${key}-${v}`;
-							});
-						}
-						return `${key}-${value}`;
-					})
-					.flat()
+				id: post.path,
+				url: `${new URL(`${post.path}`, 'https://tommyp.org').href}`,
+				title: post.meta.title,
+				summary: post.meta.subtitle,
+				date_published: post.meta.date,
+				content_text: post.content,
+				content_html: post.content
+				// tags: post.tags
+				// 	?.map((c: string | string[] | { string: string } | { string: string[] }) => {
+				// 		if (typeof c === 'string') return c;
+				// 		if (Array.isArray(c)) {
+				// 			return c.map((v) => {
+				// 				return v;
+				// 			});
+				// 		}
+				// 		const [key, value] = Object.entries(c)[0];
+				// 		if (Array.isArray(value)) {
+				// 			return value.map((v) => {
+				// 				return `${key}-${v}`;
+				// 			});
+				// 		}
+				// 		return `${key}-${value}`;
+				// 	})
+				// 	.flat()
 			};
 		})
 	};
