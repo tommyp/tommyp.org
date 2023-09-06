8d52cea8c8104c9c482584aaf3a6c4b3b7757267
Tommy Palmer <hi@tommyp.org>
2023-09-06 22:40:02 +0100

    change hero text

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,47 +1,22 @@
 <script lang="ts">
 	import Wrapper from './Wrapper.svelte';
-
-	const links = [
-		{
-			background: '#e1e600',
-			color: '#000',
-			href: 'https://castrooms.com/',
-			text: 'CastRooms',
-			subtitle: 'Work'
-		},
-		{
-			href: 'https://lineup.fun',
-			text: 'Lineup',
-			subtitle: 'Spotify Playlist Generator',
-			background: '#95fc0f',
-			color: '#fc660f'
-		},
-		{
-			href: 'https://isitshortsweathertoday.com',
-			text: 'Shorts',
-			subtitle: 'World Famous Weather App',
-			background: '#cd3f1c',
-			color: '#fff'
-		}
-	];
 </script>
 
 <section>
 	<Wrapper>
-		<p>I'm a product focussed and creative web developer. I write JavaScript and Elixir.</p>
-
-		<div>
-			<ul>
-				{#each links as link}
-					<li style:--background={link.background}>
-						<a href={link.href} target="_blank" rel="noopener noreferrer">
-							<span class="subtitle">{link.subtitle}</span>
-							<span class="text">{link.text}</span>
-						</a>
-					</li>
-				{/each}
-			</ul>
-		</div>
+		<p>
+			I'm Tommy. I'm a product focussed and creative web developer. Working on things like
+			<a href="https://castrooms.com" style:--background="#e1e600" style:--color="#e500e1"
+				>CastRooms</a
+			>,
+			<a href="https://lineup.fun" style:--background="#95fc0f" style:--color="#fc660f">Lineup</a>,
+			and
+			<a
+				href="https://isitshortsweathertoday.com"
+				style:--background="#cd3f1c"
+				style:--color="#fc3402">Shorts</a
+			>.
+		</p>
 	</Wrapper>
 </section>
 
@@ -72,29 +47,15 @@
 		gap: 1rem;
 	}
 
-	li {
-		font-size: 1.5rem;
-		width: 100%;
-	}
-	li .subtitle {
-		font-size: 1rem;
-		text-transform: uppercase;
-	}
-
 	a {
 		color: var(--near-black);
-		text-decoration: none;
-		border: 2px solid var(--near-black);
-		padding: 0.5rem;
-		display: flex;
-		flex-direction: column;
-		/* overflow: hidden; */
+		text-decoration-color: var(--background);
 		transition: all 0.25s ease-in-out;
 	}
 
 	a:hover {
 		border-radius: 0;
-		background: var(--background);
+		color: var(--color);
 	}
 
 	@media screen and (min-width: 968px) {
@@ -106,22 +67,7 @@
 
 		p {
 			margin: 1.5rem 0;
-		}
-
-		ul {
-			flex-direction: row;
-			flex-wrap: nowrap;
-			gap: 2rem;
-		}
-
-		li {
-			width: 33%;
-			font-size: 3.5rem;
-		}
-
-		li a {
-			padding: 0.5rem 1rem;
-			border-radius: 2rem;
+			/* max-width: 60vw; */
 		}
 	}
 </style>
