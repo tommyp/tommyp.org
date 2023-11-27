d049bb3af677d2c550d69ea7b51fe3acaffd9799
Tommy Palmer <hi@tommyp.org>
2023-11-27 22:57:07 +0000

    move everything up a directory

--- svelte_src/lib/components/html/H1.svelte
+++
@@ -1,9 +0,0 @@
-<h1><slot /></h1>
-
-<style>
-	h1 {
-		font-size: 3rem;
-		font-weight: bold;
-		margin-bottom: 1rem;
-	}
-</style>
--- svelte_src/routes/contact/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1>Get in touch</h1>
-
-<p><a href="mailto:hi@tommyp.org">Email me!</a></p>
