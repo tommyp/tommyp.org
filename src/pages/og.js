ea1184cf91c3a26f9afb4b6c5cdd421e09fe708f
Tommy Palmer <hi@tommyp.org>
2023-12-20 23:35:19 +0000

    some more tagline style

---
+++ src/pages/og.js
@@ -0,0 +1,45 @@
+import { html as toReactNode } from 'satori-html';
+import { Resvg } from '@resvg/resvg-js';
+
+import OpenGraph from '../components/OpenGraph.svelte';
+import satori from 'satori';
+
+const height = 630;
+const width = 1200;
+
+export const GET = async ({ url }) => {
+	const heading = url.searchParams.get('heading') ?? undefined;
+	const bgColor = url.searchParams.get('bgColor') ?? undefined;
+	const textColor = url.searchParams.get('textColor') ?? undefined;
+
+	const result = OpenGraph.render({ heading, bgColor, textColor });
+
+	const element = toReactNode(`<style>${result.css.code}</style>${result.html}`);
+
+	const svg = await satori(element, {
+		fonts: [
+			{
+				name: 'DM Sans',
+				data: Buffer.from(DMSans),
+				style: 'normal'
+			}
+		],
+		height,
+		width
+	});
+
+	const resvg = new Resvg(svg, {
+		fitTo: {
+			mode: 'width',
+			value: width
+		}
+	});
+
+	const image = resvg.render();
+
+	return new Response(image.asPng(), {
+		headers: {
+			'content-type': 'image/png'
+		}
+	});
+};
