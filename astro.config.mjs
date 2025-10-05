a1dcf712e78aa3a1cc6136c9c8a647d0e878abd7
Tommy Palmer <hi@tommyp.org>
2025-10-05 14:51:56 +0100

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'http://tommyp.org',
+	adapter: netlify(),
 	integrations: [svelte()]
 });
